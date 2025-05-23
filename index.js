441774e9a499dd3e5cbc0ad536148d1a141c5076
sdtrumpower <sdtrumpower@gmail.com>
2025-05-23 17:03:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const open = require('open');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const DS_AUTH_SERVER = 'account.docusign.com';
+const DS_AUTH_SERVER = 'account-d.docusign.com';
 const basePath = 'https://demo.docusign.net/restapi';
 
-const oauthBasePath = 'account.docusign.com';
+const oauthBasePath = 'account-d.docusign.com';
 
 const scopes = ["signature", "impersonation"];
 
