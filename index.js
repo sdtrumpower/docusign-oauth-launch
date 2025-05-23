bdf93fbb8c979ee574e3735677c267cd5c4b4678
sdtrumpower <sdtrumpower@gmail.com>
2025-05-23 15:56:03 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+{
+  "name": "docusign-oauth-launch",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "dotenv": "^16.0.3",
+    "express": "^4.18.2",
+    "open": "^8.4.0",
+    "docusign-esign": "^5.14.0"
+  }
+}
