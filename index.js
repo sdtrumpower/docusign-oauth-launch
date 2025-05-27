c727548f04ad27cff5809edbd262c255d2954c42
sdtrumpower <sdtrumpower@gmail.com>
2025-05-27 13:23:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -54,8 +54,8 @@ async function launchEnvelope(envelopeApi) {
     },
                {
       roleName: "Nurse", // Must match template role
-      name: "Nancy Nurse",
-      email: "sdtdsign+nancy@gmail.com",
+      name: "Scott Trumpower",
+      email: "scott.trumpower@docusign.com",
       recipientId: "2",           
       routingOrder: "2"
     },
